319b65dc924dbcf6dd933f2f591244ad93f3730f
luoyumo <yumo.luo@hotmail.com>
2018-11-15 15:19:11 +0200

    user can list all blogposts and find certain blogpost by name or title

--- src/main/java/ohtu/dao/BlogpostDao.java
+++ src/main/java/ohtu/dao/BlogpostDao.java
@@ -1,37 +1,86 @@
 package ohtu.dao;
 
+import ohtu.domain.Bookmark;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import ohtu.database.Database;
 import ohtu.domain.Blogpost;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
- * A DAO object for the Blogposts. This class talks with the ohtu.database and creates (or updates/deletes) Blogpost objects.
+ * A DAO object for the Blogposts. This class talks with the ohtu.database and
+ * creates (or updates/deletes) Blogpost objects.
  */
 public class BlogpostDao implements ObjectDao<Blogpost, Integer> {
 
-	private final Database database;
+    private final Database database;
 
-	public BlogpostDao(Database database) {
-		this.database = database;
-	}
+    public BlogpostDao(Database database) {
+        this.database = database;
+    }
 
-	// TODO: Implement findById, findByTitle, findAll etc.
+    // TODO: Implement findById, findByTitle, findAll etc.
+    @Override
+    public Blogpost findByTitle(String title) throws SQLException, ParseException {
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT*FROM bookmark,blogpost WHERE bookmark.title=? AND bookmark.id=blogpost.id");
+        stmt.setString(1, title);
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            Blogpost output = constructBlogpostFromResultSet(rs);
+            rs.close();
+            stmt.close();
+            conn.close();
+            return output;
+        }
+        return null;
+    }
 
-	@Override
-	public Blogpost findByTitle(String title) throws SQLException {
-		return null;
-	}
+    @Override
+    public List<Blogpost> findAll() throws SQLException, ParseException {
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT*FROM bookmark,blogpost WHERE bookmark.id=blogpost.id");
+        ResultSet rs = stmt.executeQuery();
+        List<Blogpost> outputs = new ArrayList<>();
+        while (rs.next()) {
+            Blogpost blogpost = constructBlogpostFromResultSet(rs);
+            outputs.add(blogpost);
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+        return outputs;
+    }
 
-	@Override
-	public List<Blogpost> findAll() throws SQLException {
-		return null;
-	}
+    @Override
+    public Blogpost findById(Integer id) throws SQLException, ParseException {
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT*FROM bookmark,blogpost WHERE bookmark.id=? AND bookmark.id=blogpost.id");
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            Blogpost output = constructBlogpostFromResultSet(rs);
+            rs.close();
+            stmt.close();
+            conn.close();
+            return output;
+        }
+        return null;
+    }
 
-	@Override
-	public Blogpost findById(Integer id) throws SQLException {
-		return null;
-	}
+    private Blogpost constructBlogpostFromResultSet(ResultSet rs) throws SQLException, ParseException {
+        String dateString = rs.getString("addDate");
+        Date dateUtil = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+        java.sql.Date date = new java.sql.Date(dateUtil.getTime());
+        Blogpost blogpost = new Blogpost(rs.getInt("id"), rs.getString("title"), date, rs.getString("author"), rs.getString("url"));
+        return blogpost;
+    }
 
 }
--- src/main/java/ohtu/dao/ObjectDao.java
+++ src/main/java/ohtu/dao/ObjectDao.java
@@ -1,22 +1,23 @@
 package ohtu.dao;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 
 /**
  * An interface which all DAO's must use.
+ *
  * @param <Bookmark> The specific type of Bookmark to be used.
  */
 public interface ObjectDao<Bookmark, Integer> {
 
-	List<Bookmark> findAll() throws SQLException;
+    List<Bookmark> findAll() throws SQLException, ParseException;
 
-	Bookmark findById(Integer id) throws SQLException;
+    Bookmark findById(Integer id) throws SQLException, ParseException;
 
-	Bookmark findByTitle(String title) throws SQLException;
+    Bookmark findByTitle(String title) throws SQLException, ParseException;
 
-	// To be implemented later:
-
-	//public void delete(Integer id) throws SQLException;
-	//public void save(Bookmark bookmark) throws SQLException;
+    // To be implemented later:
+    //public void delete(Integer id) throws SQLException;
+    //public void save(Bookmark bookmark) throws SQLException;
 }
