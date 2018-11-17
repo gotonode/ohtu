22858b4e129687854b3adef8ab89381f6546a410
Viannaiv <viva95@windowslive.com>
2018-11-17 18:00:00 +0200

    Modified the CREATE TABLE -statements

--- src/main/resources/createstatements.sql
+++ src/main/resources/createstatements.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE Bookmark ( 
     id INTEGER PRIMARY KEY, 
-    title varchar(100),
+    title varchar(200),
     addDate datetime default current_timestamp,
     type varchar(1)
 );  
 
 CREATE TABLE Blogpost (
     id integer,  
-    author varchar(50),
-    url varchar(100),                                                                                                           
+    author varchar(100),
+    url varchar(400),                                                                                                           
     FOREIGN KEY (id) REFERENCES bookmark(id)); 
