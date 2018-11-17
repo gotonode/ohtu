c72cea2ece1ed195dff34125975c5cd1d87e08f5
Viannaiv <viva95@windowslive.com>
2018-11-18 01:18:37 +0200

    Moved createstatements.sql into directory src/main/resources/sql

---
+++ src/main/resources/sql/createstatements.sql
@@ -0,0 +1,19 @@
+/* These are the CREATE TABLE -statements for the database used by this application */
+/* Use only one line comments in this file, with the same comment markings as the ones used here or starting the line with // */
+
+CREATE TABLE IF NOT EXISTS Bookmark (
+    id INTEGER PRIMARY KEY,
+    title varchar(200),
+    addDate datetime default current_timestamp,
+    type varchar(1)
+);
+
+CREATE TABLE IF NOT EXISTS Blogpost (
+    id integer NOT NULL,
+    author varchar(100),
+    url varchar(400),
+    FOREIGN KEY (id) REFERENCES Bookmark(id)
+);
+
+/* TODO: Consider adding NOT NULL for some of the columns */
+/* TODO: Consider changing some varchar sizes */
