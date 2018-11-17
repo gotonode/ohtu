0e63958743d35fe6acbf372d9d44b15aedc2c7aa
Viannaiv <viva95@windowslive.com>
2018-11-18 01:22:27 +0200

    Modified Database to create a new database (when needed) and create the database structure according to createstatements.sql

--- src/main/java/ohtu/database/Database.java
+++ src/main/java/ohtu/database/Database.java
@@ -1,9 +1,9 @@
 package ohtu.database;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.FileReader;
+import java.sql.*;
 
 public class Database {
 
@@ -12,14 +12,66 @@ public class Database {
 
 	/**
 	 * Creates a new Database-object with the given File, which points to the actual SQLite ohtu.database.
+         * If the given database file does not already exist it is created with the intended database structure
+         * defined as a sql-script in /src/main/resources/sql/createstatements.sql
+         * 
 	 * @param file Where the SQLite ohtu.database file is located.
 	 */
 	public Database(File file) {
 		this.databaseFile = file;
 		this.databaseAddress = ("jdbc:sqlite:").concat(file.getAbsolutePath());
+                
+                if (!this.databaseFile.exists()) {
+                    initiateDatabaseTables();
+                }
 	}
-
+        
+        /**
+         * Get connection to database.
+         * 
+         * @return Connection to database.
+         * @throws SQLException 
+         */
 	public Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(databaseAddress);
 	}
+        
+        /**
+         * Creates the database structure for a new database from a SQL-script file.
+         */
+        private void initiateDatabaseTables(){
+            try {
+                runSQLScript(System.getProperty("user.dir") + "/src/main/resources/sql/createstatements.sql");
+            } catch (SQLException e) {
+                // Later switch from printing in console to printing in log?
+                System.out.println("Error:" + e.toString());
+            }
+        }
+        
+        private void runSQLScript(String filepath) throws SQLException {
+            try(Connection conn = getConnection(); Statement s = conn.createStatement()) {
+                BufferedReader reader = new BufferedReader(new FileReader(filepath));
+
+                String executable = "";
+                String nextline;
+                
+                while ((nextline = reader.readLine()) != null) {
+                    String line = nextline.trim();
+                    
+                    if (line.equals(("")) || line.startsWith("//") || line.startsWith("/*")) {
+                            continue;
+                    }
+                    
+                    executable += line + " ";
+
+                    if (line.endsWith(";")) {
+                            s.executeUpdate(executable);
+                            executable = "";
+                    }
+                } 
+            } catch(Exception e) {
+                // Later switch from printing in console to printing in log?
+                System.out.println("Error: " + e.toString());
+            }
+        }  
 }
