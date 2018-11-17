d8feb7fa5557409b9ba31442dec402df75d9ba39
Viannaiv <viva95@windowslive.com>
2018-11-18 01:31:17 +0200

    Main now creates/utilises bookmarks.db in the root directory of the app and gives it to be used by App

--- src/main/java/ohtu/main/Main.java
+++ src/main/java/ohtu/main/Main.java
@@ -1,8 +1,12 @@
 package ohtu.main;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
 import ohtu.io.ConsoleIO;
-
 import java.util.Scanner;
+import ohtu.database.Database;
 
 /**
  * The program starts here. Program logic is handled in App-object. This class is ignored in code coverage.
@@ -15,7 +19,10 @@ public class Main {
 
 	private static App app;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException, ParseException {
+   
+        File databaseFile = new File(System.getProperty("user.dir") + "/bookmarks.db");
+        Database db = new Database(databaseFile);
 
     	if (DEBUG) {
 			System.out.println("# App started in DEBUG mode");
@@ -25,7 +32,7 @@ public class Main {
 
     	ConsoleIO consoleIo = new ConsoleIO(scanner);
 
-    	app = new App(consoleIo); // Dependency injection. For tests, pass a StubIO.
+    	app = new App(consoleIo, db); // Dependency injection. For tests, pass a StubIO.
 		app.run(); // Run the app. It contains a loop, and once it breaks, the app will terminate.
     }
 }
