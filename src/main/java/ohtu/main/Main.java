9db0bc41d21e98147b504b7b2e19d71ab2811801
gotonode <5214152+gotonode@users.noreply.github.com>
2018-11-21 17:41:30 +0200

    Tidy up Main

--- src/main/java/ohtu/main/Main.java
+++ src/main/java/ohtu/main/Main.java
@@ -1,8 +1,9 @@
 package ohtu.main;
 
 import java.io.File;
-import java.text.ParseException;
+
 import ohtu.io.ConsoleIO;
+
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -13,30 +14,24 @@ import ohtu.database.Database;
  */
 public class Main {
 
-    public static final Logger LOG = Logger.getLogger(Main.class.getName());
+	// Use the logger for error messages and other such important topics.
+	public static final Logger LOG = Logger.getLogger(Main.class.getName());
 
-    // Declare final variables here.
-    public static final String APP_NAME = "Bookmarks Database";
+	// Declare final variables here.
+	public static final String APP_NAME = "Bookmarks Database";
 
-    // Only enable the following temporarily if needed, and then disable it before committing.
-    public static final boolean DEBUG = false; // Will cause StubIO to print its contents, useful for tests.
+	private static App app;
 
-    private static App app;
+	public static void main(String[] args) {
 
-    public static void main(String[] args) throws ParseException {
-   
-        File databaseFile = new File(System.getProperty("user.dir") + "/bookmarks.db");
-        Database db = new Database(databaseFile);
+		File databaseFile = new File(System.getProperty("user.dir") + "/bookmarks.db");
+		Database db = new Database(databaseFile);
 
-    	if (DEBUG) {
-			Main.LOG.info("# App started in DEBUG mode");
-		}
+		Scanner scanner = new Scanner(System.in);
 
-    	Scanner scanner = new Scanner(System.in);
+		ConsoleIO consoleIo = new ConsoleIO(scanner);
 
-    	ConsoleIO consoleIo = new ConsoleIO(scanner);
-
-    	app = new App(consoleIo, db); // Dependency injection. For tests, pass a StubIO.
+		app = new App(consoleIo, db); // Dependency injection. For tests, pass a StubIO.
 		app.run(); // Run the app. It contains a loop, and once it breaks, the app will terminate.
-    }
+	}
 }
