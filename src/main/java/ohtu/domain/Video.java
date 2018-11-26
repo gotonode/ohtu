1ece5ba7ff3a79b406f2275c4b7726f8124c8bb9
Viannaiv <viva95@windowslive.com>
2018-11-26 21:52:41 +0200

    Added equals and hashcode methods to Video

--- src/main/java/ohtu/domain/Video.java
+++ src/main/java/ohtu/domain/Video.java
@@ -1,28 +1,45 @@
 package ohtu.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Video extends Bookmark {
 
-	private String url;
+    private String url;
 
-	public Video(int id, String title, Date addDate, String url) {
-		super(id, title, addDate, 'V'); // V for video.
-		this.url = url;
-	}
+    public Video(int id, String title, Date addDate, String url) {
+            super(id, title, addDate, 'V'); // V for video.
+            this.url = url;
+    }
 
-	@Override
-	public String toString() {
-		return "Video{" +
-				"url='" + url + '\'' +
-				'}';
-	}
+    @Override
+    public String toString() {
+            return "Video{" +
+                            "url='" + url + '\'' +
+                            '}';
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+            return url;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+            this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!this.getClass().equals(object.getClass())) {
+            return false;
+        }
+
+        return this.getUrl().equals(((Video)object).getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 9;
+        hash = 37 * hash + Objects.hashCode(this.url);
+        return hash;
+    }
 }
