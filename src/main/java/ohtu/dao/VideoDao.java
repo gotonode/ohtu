e7a431022851046c17b7a566b8544889cf076854
Viannaiv <viva95@windowslive.com>
2018-11-28 21:39:59 +0200

    Modified methods in BookmarkDao and added Javadocs

--- src/main/java/ohtu/dao/VideoDao.java
+++ src/main/java/ohtu/dao/VideoDao.java
@@ -149,6 +149,15 @@ public class VideoDao implements ObjectDao<Video, Integer> {
         return false;
     }
     
+    /**
+     * Constructs a Video object from a ResultSet. The fields of the created Video object match
+     * the corresponding data found in the ResultSet.
+     * 
+     * @param rs the ResultSet containing the needed information
+     * @return A Video object constructed from the data in the given ResultSet
+     * @throws SQLException
+     * @throws ParseException 
+     */
     private Video constructVideoFromResultSet(ResultSet rs) throws SQLException, ParseException {
         Date date = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(rs.getString("addDate")).getTime());
         Video video = new Video(rs.getInt("id"), rs.getString("title"), date, rs.getString("url"));
@@ -156,6 +165,12 @@ public class VideoDao implements ObjectDao<Video, Integer> {
         return video;
     }
     
+    /**
+     * Returns the last id added to the database.
+     * 
+     * @return latest used id or -1 if unable to retrieve id.
+     * @throws SQLException 
+     */
     private int getLatestId() throws SQLException{
         String s = "SELECT MAX(id) FROM bookmark";
         
@@ -168,6 +183,14 @@ public class VideoDao implements ObjectDao<Video, Integer> {
         return -1;
     }
     
+    /**
+     * Retrieves all information of Video objects stored in the database and creates a list 
+     * of corresponding Video objects ordered alphabetically by title.
+     * 
+     * @return alphabetically ordered (by title) list of Video objects.
+     * @throws SQLException
+     * @throws ParseException 
+     */
     public List<Video> findAllOrderByTitle() throws SQLException, ParseException {
         String s = "SELECT * FROM bookmark, video WHERE bookmark.id = video.id ORDER BY title";
         List<Video> videos = new ArrayList<>();
