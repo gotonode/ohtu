ef3f60f6ed2cdd18fe347da2f340452f4601fd83
gotonode <5214152+gotonode@users.noreply.github.com>
2018-12-03 14:02:48 +0200

    App now reads SQL from within the JAR

--- src/main/java/ohtu/database/SQLReader.java
+++ src/main/java/ohtu/database/SQLReader.java
@@ -1,76 +1,50 @@
 
 package ohtu.database;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SQLReader {
-    private final String filepath;
+class SQLReader {
+	private final String path;
 
-    /**
-     * Creates a new SQLReader-object that can be utilised to read the contents of the 
-     * SQL-script file located at the given filepath.
-     * @param filepath 
-     */
-    public SQLReader(String filepath) {
-        this.filepath = filepath;
-    }
-    
-    /**
-     * Reads the SQL-statements in the file that is found through the
-     * given filepath.
-     * 
-     * @return List of SQL-statements ready to be executed
-     * @throws FileNotFoundException
-     * @throws IOException 
-     */
-    public List<String> readSQLStatements() throws IOException {
-        if (!new File(filepath).exists()) {
-            return backupCreateTableStatements();
-        }
-        
-        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+	/**
+	 * Creates a new SQLReader-object that can be utilised to read the contents of the
+	 * SQL-script file located at the given path.
+	 *
+	 * @param path The path to the internal SQL file.
+	 */
+	SQLReader(String path) {
+		this.path = path;
+	}
 
-        List<String> statements = new ArrayList<>();
-        String statement = "";
-        String nextline;
+	/**
+	 * Reads the SQL-statements in the file that is found through the
+	 * given path.
+	 *
+	 * @return List of SQL-statements ready to be executed.
+	 */
+	List<String> readSQLStatements() {
+		String s = loadStreamData(path);
 
-        while ((nextline = reader.readLine()) != null) {
-            String line = nextline.trim();
+		String[] lines = s.split("\n");
+		ArrayList<String> output = new ArrayList<>();
+		for (String line : lines) {
+			output.add(line.trim());
+		}
+		return output;
+	}
 
-            if (line.equals(("")) || line.startsWith("//") || line.startsWith("/*")) {
-                continue;
-            }
+	/**
+	 * Loads data from an internal resource.
+	 *
+	 * @param internalPath The internal path to the resource.
+	 * @return A String-object containing the loaded data. Has linefeeds ("\n").
+	 */
+	private String loadStreamData(String internalPath) {
+		InputStream inputStream = getClass().getResourceAsStream(internalPath);
+		return new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+	}
 
-            statement += line + " "; // TODO: Use a StringBuilder here?
-
-            if (line.endsWith(";")) {
-                statements.add(statement);
-                statement = "";
-            }
-        }
-        
-        return statements;
-    }
-    
-    private List<String> backupCreateTableStatements() {
-        List<String> statements = new ArrayList<>();
-        
-        statements.add("CREATE TABLE IF NOT EXISTS Bookmark (id INTEGER PRIMARY KEY, " +    
-                "title varchar(200), addDate datetime default current_timestamp, type varchar(1));");
-        statements.add("CREATE TABLE IF NOT EXISTS Blogpost (id integer NOT NULL, author varchar(100), " +
-                "url varchar(400), PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES Bookmark(id));");
-        statements.add("CREATE TABLE IF NOT EXISTS Video (id integer NOT NULL, url varchar(400), " +
-                "PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES Bookmark(id));");
-        statements.add("CREATE TABLE IF NOT EXISTS Book (id integer NOT NULL, author varchar(100), " +
-                "isbn varchar(20), PRIMARY KEY (id), FOREIGN KEY (id) REFERENCES Bookmark(id));");
-        
-        return statements;
-    }
-    
 }
