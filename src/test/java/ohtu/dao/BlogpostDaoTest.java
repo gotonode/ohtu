3e9f26b27d6bbbc3dcc1698a057c057b452800e8
luoyumo <yumo.luo@hotmail.com>
2018-12-04 03:10:36 +0200

    unit tests for findByURL-method

--- src/test/java/ohtu/dao/BlogpostDaoTest.java
+++ src/test/java/ohtu/dao/BlogpostDaoTest.java
@@ -19,115 +19,122 @@ import static org.junit.Assert.*;
 
 public class BlogpostDaoTest extends AbstractDaoTest {
 
-	private static BlogpostDao blogpostDao;
-	private static Blogpost b1, b2, b3;
+    private static BlogpostDao blogpostDao;
+    private static Blogpost b1, b2, b3;
 
-	@BeforeClass
-	public static void setUpClass() throws IOException {
-		initialize();
-	}
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        initialize();
+    }
 
-	@AfterClass
-	public static void tearDownClass() {
-		databaseFile.delete();
-	}
+    @AfterClass
+    public static void tearDownClass() {
+        databaseFile.delete();
+    }
 
-	@Before
-	public void setUp() throws SQLException, ParseException, IOException {
+    @Before
+    public void setUp() throws SQLException, ParseException, IOException {
 
-		// Can we delete the database after each test and re-create it? Currently the following doesn't work.
-		final boolean delete = databaseFile.delete();
+        // Can we delete the database after each test and re-create it? Currently the following doesn't work.
+        final boolean delete = databaseFile.delete();
 
-		// TODO: This temporary fixes the problem on Windows. But it's ineffective. And creates a lot of temp folders.
-		setUpClass();
+        // TODO: This temporary fixes the problem on Windows. But it's ineffective. And creates a lot of temp folders.
+        setUpClass();
 
-		database = new Database(databaseFile);
-		blogpostDao = new BlogpostDao(database);
-		b1 = new Blogpost(-1, "Data Mining", null, "navamani saravanan", "http://notescompsci.blogspot.com/2013/04/data-mining.html");
-		b2 = new Blogpost(-1, "Data Mining For Beginners", null, "P.Fournier-Viger", "http://data-mining.philippe-fournier-viger.com/introduction-data-mining");
-		b3 = new Blogpost(-1, "Java TWO marks", null, "navamani saravanan", "http://notescompsci.blogspot.com/2013/04/java-two-marks.html");
+        database = new Database(databaseFile);
+        blogpostDao = new BlogpostDao(database);
+        b1 = new Blogpost(-1, "Data Mining", null, "navamani saravanan", "http://notescompsci.blogspot.com/2013/04/data-mining.html");
+        b2 = new Blogpost(-1, "Data Mining For Beginners", null, "P.Fournier-Viger", "http://data-mining.philippe-fournier-viger.com/introduction-data-mining");
+        b3 = new Blogpost(-1, "Java TWO marks", null, "navamani saravanan", "http://notescompsci.blogspot.com/2013/04/java-two-marks.html");
 
-		blogpostDao.create(b1);
-		blogpostDao.create(b2);
-		blogpostDao.create(b3);
-	}
+        blogpostDao.create(b1);
+        blogpostDao.create(b2);
+        blogpostDao.create(b3);
+    }
 
-	@After
-	public void tearDown() {
-		databaseFile.delete();
-	}
+    @After
+    public void tearDown() {
+        databaseFile.delete();
+    }
 
-	@Test
-	public void canCreateNewBlogpost() throws SQLException, ParseException {
-		Blogpost b4 = new Blogpost(-1, "How to learn computer science", null, "Karim", "https://www.afternerd.com/blog/learn-computer-science/");
-		Blogpost added = blogpostDao.create(b4);
-		assertEquals(b4, added);
-	}
+    @Test
+    public void canCreateNewBlogpost() throws SQLException, ParseException {
+        Blogpost b4 = new Blogpost(-1, "How to learn computer science", null, "Karim", "https://www.afternerd.com/blog/learn-computer-science/");
+        Blogpost added = blogpostDao.create(b4);
+        assertEquals(b4, added);
+    }
 
-	@Test
-	public void canListBlogpostsWithSimilarTitle() throws SQLException, ParseException {
-		String keyword = "data mining";
-		List<Blogpost> blogposts = blogpostDao.findByTitle(keyword);
-		assertEquals(2, blogposts.size());
-	}
+    @Test
+    public void canListBlogpostsWithSimilarTitle() throws SQLException, ParseException {
+        String keyword = "data mining";
+        List<Blogpost> blogposts = blogpostDao.findByTitle(keyword);
+        assertEquals(2, blogposts.size());
+    }
 
-	@Test
-	public void canListAllBlogposts() throws SQLException, ParseException {
-		List<Blogpost> allBlogposts = blogpostDao.findAll();
-		assertEquals(3, allBlogposts.size());
-	}
+    @Test
+    public void canListBlogpostsWithSimilarUrl() throws SQLException, ParseException {
+        String keyword = "notescompsci";
+        List<Blogpost> blogposts = blogpostDao.findByURL(keyword);
+        assertEquals(2, blogposts.size());
+    }
 
-	@Test
-	public void canFindBlogpostByExistedId() throws SQLException, ParseException {
-		Blogpost found = blogpostDao.findById(1);
-		assertEquals(1, found.getId());
-	}
+    @Test
+    public void canListAllBlogposts() throws SQLException, ParseException {
+        List<Blogpost> allBlogposts = blogpostDao.findAll();
+        assertEquals(3, allBlogposts.size());
+    }
 
-	@Test
-	public void cannotFindBlogpostByNonexistedId() throws SQLException, ParseException {
-		Blogpost found = blogpostDao.findById(100);
-		assertNull(found);
-	}
+    @Test
+    public void canFindBlogpostByExistedId() throws SQLException, ParseException {
+        Blogpost found = blogpostDao.findById(1);
+        assertEquals(1, found.getId());
+    }
 
-	@Test
-	public void canDeleteBlogpostByExistedId() throws SQLException, ParseException {
-		int toBeDeleted = 1;
-		boolean success = blogpostDao.delete(toBeDeleted);
-		assertTrue(success);
-		assertNull(blogpostDao.findById(toBeDeleted));
-	}
+    @Test
+    public void cannotFindBlogpostByNonexistedId() throws SQLException, ParseException {
+        Blogpost found = blogpostDao.findById(100);
+        assertNull(found);
+    }
 
-	@Test
-	public void cannotDeleteBlogpostByNonexistedId() throws SQLException {
-		int nonExisted = 100;
-		boolean success = blogpostDao.delete(nonExisted);
-		assertFalse(success);
-	}
+    @Test
+    public void canDeleteBlogpostByExistedId() throws SQLException, ParseException {
+        int toBeDeleted = 1;
+        boolean success = blogpostDao.delete(toBeDeleted);
+        assertTrue(success);
+        assertNull(blogpostDao.findById(toBeDeleted));
+    }
 
-	@Test
-	public void canUpdateExistedBlogpost() throws SQLException, ParseException {
-		Blogpost toBeUpdated = blogpostDao.findById(1);
-		String newTitle = "Data mining for beginners";
-		toBeUpdated.setTitle(newTitle);
-		boolean success = blogpostDao.update(toBeUpdated);
-		assertTrue(success);
-		assertEquals(newTitle, blogpostDao.findById(toBeUpdated.getId()).getTitle());
-	}
+    @Test
+    public void cannotDeleteBlogpostByNonexistedId() throws SQLException {
+        int nonExisted = 100;
+        boolean success = blogpostDao.delete(nonExisted);
+        assertFalse(success);
+    }
 
-	@Test
-	public void cannotUpdateNonexistedBlogpost() throws SQLException {
-		Blogpost nonExisted = new Blogpost(100, "Java Base64 URL Safe Encoding ", null, "Billy Yarosh", "https://keaplogik.blogspot.com/2016/01/java-base64-url-safe-encoding.html");
-		nonExisted.setTitle("Safe Encoding for beginners");
-		boolean success = blogpostDao.update(nonExisted);
-		assertFalse(success);
-	}
+    @Test
+    public void canUpdateExistedBlogpost() throws SQLException, ParseException {
+        Blogpost toBeUpdated = blogpostDao.findById(1);
+        String newTitle = "Data mining for beginners";
+        toBeUpdated.setTitle(newTitle);
+        boolean success = blogpostDao.update(toBeUpdated);
+        assertTrue(success);
+        assertEquals(newTitle, blogpostDao.findById(toBeUpdated.getId()).getTitle());
+    }
 
-	@Test
-	public void canFindAllBlogpostsOrderedByTitle() throws SQLException, ParseException {
-		List<Blogpost> allBlogposts = blogpostDao.findAllOrderByTitle();
-		assertEquals(b1.getTitle(), allBlogposts.get(0).getTitle());
-		assertEquals(b2.getTitle(), allBlogposts.get(1).getTitle());
-		assertEquals(b3.getTitle(), allBlogposts.get(2).getTitle());
-	}
+    @Test
+    public void cannotUpdateNonexistedBlogpost() throws SQLException {
+        Blogpost nonExisted = new Blogpost(100, "Java Base64 URL Safe Encoding ", null, "Billy Yarosh", "https://keaplogik.blogspot.com/2016/01/java-base64-url-safe-encoding.html");
+        nonExisted.setTitle("Safe Encoding for beginners");
+        boolean success = blogpostDao.update(nonExisted);
+        assertFalse(success);
+    }
+
+    @Test
+    public void canFindAllBlogpostsOrderedByTitle() throws SQLException, ParseException {
+        List<Blogpost> allBlogposts = blogpostDao.findAllOrderByTitle();
+        assertEquals(b1.getTitle(), allBlogposts.get(0).getTitle());
+        assertEquals(b2.getTitle(), allBlogposts.get(1).getTitle());
+        assertEquals(b3.getTitle(), allBlogposts.get(2).getTitle());
+    }
 
 }
--- src/test/java/ohtu/dao/BookmarkDaoTest.java
+++ src/test/java/ohtu/dao/BookmarkDaoTest.java
@@ -1,4 +1,3 @@
-
 package ohtu.dao;
 
 import java.io.File;
@@ -19,85 +18,91 @@ import org.junit.rules.TemporaryFolder;
 
 import static org.junit.Assert.*;
 
-
 public class BookmarkDaoTest {
 
-	private static TemporaryFolder tempFolder;
+    private static TemporaryFolder tempFolder;
 
-	private static Database database;
-	private static File databaseFile;
-	private static BlogpostDao blogpostDao;
-	private static BookmarkDao bookmarkDao;
-	private static VideoDao videoDao;
-	private static BookDao bookDao;
-	private static Blogpost b1;
-	private static Video v1, v2;
-	private static Book k1;
+    private static Database database;
+    private static File databaseFile;
+    private static BlogpostDao blogpostDao;
+    private static BookmarkDao bookmarkDao;
+    private static VideoDao videoDao;
+    private static BookDao bookDao;
+    private static Blogpost b1;
+    private static Video v1, v2;
+    private static Book k1;
 
-	@BeforeClass
-	public static void setUpClass() throws SQLException, ParseException, IOException {
+    @BeforeClass
+    public static void setUpClass() throws SQLException, ParseException, IOException {
 
-		tempFolder = new TemporaryFolder();
-		tempFolder.create();
+        tempFolder = new TemporaryFolder();
+        tempFolder.create();
 
-		// Assign a test database into the newly created temporary folder.
-		databaseFile = new File(tempFolder.getRoot() + "/test.db");
+        // Assign a test database into the newly created temporary folder.
+        databaseFile = new File(tempFolder.getRoot() + "/test.db");
 
-		if (databaseFile.exists()) {
-			databaseFile.delete();
-		}
-		BookmarkDaoTest.database = new Database(databaseFile);
-		BookmarkDaoTest.blogpostDao = new BlogpostDao(database);
-		BookmarkDaoTest.videoDao = new VideoDao(database);
-		BookmarkDaoTest.bookDao = new BookDao(database);
-		BookmarkDaoTest.bookmarkDao = new BookmarkDao(database, blogpostDao, videoDao, bookDao);
-		BookmarkDaoTest.b1 = new Blogpost(-1, "Data Mining", null, "navamani saravanan", "http://notescompsci.blogspot.com/2013/04/data-mining.html");
-		BookmarkDaoTest.v1 = new Video(-1, "Map of Computer Science", null, "https://www.youtube.com/watch?v=SzJ46YA_RaA");
-		BookmarkDaoTest.v2 = new Video(-1, "Big Data Analytics", null, "https://www.youtube.com/watch?v=LtScY2guZpo");
-		BookmarkDaoTest.k1 = new Book(-1, "Introdiction to Algorithms", null, "Thomas H. Cormen", "9-780-262-0338-48");
-		blogpostDao.create(b1);
-		videoDao.create(v1);
-		videoDao.create(v2);
-		bookDao.create(k1);
-	}
+        if (databaseFile.exists()) {
+            databaseFile.delete();
+        }
+        BookmarkDaoTest.database = new Database(databaseFile);
+        BookmarkDaoTest.blogpostDao = new BlogpostDao(database);
+        BookmarkDaoTest.videoDao = new VideoDao(database);
+        BookmarkDaoTest.bookDao = new BookDao(database);
+        BookmarkDaoTest.bookmarkDao = new BookmarkDao(database, blogpostDao, videoDao, bookDao);
+        BookmarkDaoTest.b1 = new Blogpost(-1, "Data Mining", null, "navamani saravanan", "http://notescompsci.blogspot.com/2013/04/data-mining.html");
+        BookmarkDaoTest.v1 = new Video(-1, "Map of Computer Science", null, "https://www.youtube.com/watch?v=SzJ46YA_RaA");
+        BookmarkDaoTest.v2 = new Video(-1, "Big Data Analytics", null, "https://www.youtube.com/watch?v=LtScY2guZpo");
+        BookmarkDaoTest.k1 = new Book(-1, "Introdiction to Algorithms", null, "Thomas H. Cormen", "9-780-262-0338-48");
+        blogpostDao.create(b1);
+        videoDao.create(v1);
+        videoDao.create(v2);
+        bookDao.create(k1);
+    }
 
-	@AfterClass
-	public static void tearDownClass() {
-		databaseFile.delete();
-	}
+    @AfterClass
+    public static void tearDownClass() {
+        databaseFile.delete();
+    }
 
-	@Test
-	public void canListAllBookmarks() throws SQLException, ParseException {
-		List<Bookmark> bookmarks = bookmarkDao.findAll();
-		assertEquals(4, bookmarks.size());
-	}
+    @Test
+    public void canListAllBookmarks() throws SQLException, ParseException {
+        List<Bookmark> bookmarks = bookmarkDao.findAll();
+        assertEquals(4, bookmarks.size());
+    }
 
-	@Test
-	public void canFindBookmarkByExistedId() throws SQLException, ParseException {
-		Bookmark found = bookmarkDao.findById(1);
-		assertTrue(found.isBlogpost());
-		assertEquals(1, found.getId());
-	}
+    @Test
+    public void canFindBookmarkByExistedId() throws SQLException, ParseException {
+        Bookmark found = bookmarkDao.findById(1);
+        assertTrue(found.isBlogpost());
+        assertEquals(1, found.getId());
+    }
 
-	@Test
-	public void canFindBookmarkByNonexistedId() throws SQLException, ParseException {
-		Bookmark found = bookmarkDao.findById(100);
-		assertNull(found);
-	}
+    @Test
+    public void canFindBookmarkByNonexistedId() throws SQLException, ParseException {
+        Bookmark found = bookmarkDao.findById(100);
+        assertNull(found);
+    }
 
-	@Test
-	public void canFindBookmarkWithSamilarTitle() throws SQLException, ParseException {
-		String keyword = "data";
-		List<Bookmark> bookmarks = bookmarkDao.findByTitle(keyword);
-		assertEquals(2, bookmarks.size());
-	}
+    @Test
+    public void canListBookmarkWithSimilarTitle() throws SQLException, ParseException {
+        String keyword = "data";
+        List<Bookmark> bookmarks = bookmarkDao.findByTitle(keyword);
+        assertEquals(2, bookmarks.size());
+    }
 
-	@Test
-	public void canFindAllBookmarksOrderByTitle() throws SQLException, ParseException {
-		List<Bookmark> allBookmarks = bookmarkDao.findAllOrderByTitle();
-		assertEquals(v2.getTitle(), allBookmarks.get(0).getTitle());
-		assertEquals(b1.getTitle(), allBookmarks.get(1).getTitle());
-		assertEquals(k1.getTitle(), allBookmarks.get(2).getTitle());
-		assertEquals(v1.getTitle(), allBookmarks.get(3).getTitle());
-	}
+    @Test
+    public void canListBookmarkWithSimilarUrl() throws SQLException, ParseException {
+        String keyword = "youtube";
+        List<Bookmark> bookmarks = bookmarkDao.findByURL(keyword);
+        assertEquals(2, bookmarks.size());
+    }
+
+    @Test
+    public void canFindAllBookmarksOrderByTitle() throws SQLException, ParseException {
+        List<Bookmark> allBookmarks = bookmarkDao.findAllOrderByTitle();
+        assertEquals(v2.getTitle(), allBookmarks.get(0).getTitle());
+        assertEquals(b1.getTitle(), allBookmarks.get(1).getTitle());
+        assertEquals(k1.getTitle(), allBookmarks.get(2).getTitle());
+        assertEquals(v1.getTitle(), allBookmarks.get(3).getTitle());
+    }
 }
