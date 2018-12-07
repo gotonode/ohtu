643785addd10d28acaa4a095efaed48692879441
luoyumo <yumo.luo@hotmail.com>
2018-12-07 15:40:31 +0200

    common steps which will be used in many scenarios

---
+++ src/test/java/ohtu/cucumberTest/successfulRegisterAndLoginStepdefs.java
@@ -0,0 +1,23 @@
+package ohtu.cucumberTest;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import java.io.IOException;
+
+public class successfulRegisterAndLoginStepdefs extends AbstractStepdefs {
+
+    @Before
+    public void before() throws IOException {
+        initialize();
+        inputs.clear();
+    }
+
+    @Given("^user has registered and logged in with username \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_has_registered_and_logged_in_with_username_and_password(String username, String password) throws Throwable {
+        inputs.add("R");
+        inputs.add(username);
+        inputs.add(password);
+        inputs.add(password);
+    }
+
+}
