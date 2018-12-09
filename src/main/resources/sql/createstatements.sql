208e9ab6c977fb9aee5d9f579dd33635b4597be2
Viannaiv <viva95@windowslive.com>
2018-12-09 02:48:26 +0200

    Added table User and foreign key referencing user to table Bookmark

--- src/main/resources/sql/createstatements.sql
+++ src/main/resources/sql/createstatements.sql
@@ -1,35 +1,40 @@
 /* These are the CREATE TABLE -statements for the database used by this application */
 /* Use only one line comments in this file, with the same comment markings as the ones used here or starting the line with // */
 
+CREATE TABLE IF NOT EXISTS User (
+    id INTEGER PRIMARY KEY,
+    username varchar(100),
+    password char(128)
+);
+
 CREATE TABLE IF NOT EXISTS Bookmark (
-	id INTEGER PRIMARY KEY,
-	title varchar(200),
-	addDate datetime default current_timestamp,
-	type varchar(1)
+    id INTEGER PRIMARY KEY,
+    user_id integer NOT NULL,
+    title varchar(200),
+    addDate datetime default current_timestamp,
+    type varchar(1),
+    FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
 CREATE TABLE IF NOT EXISTS Blogpost (
-	id integer NOT NULL,
-	author varchar(100),
-	url varchar(400),
-	PRIMARY KEY (id),
-	FOREIGN KEY (id) REFERENCES Bookmark(id)
+    id integer NOT NULL,
+    author varchar(100),
+    url varchar(400),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES Bookmark(id)
 );
 
 CREATE TABLE IF NOT EXISTS Video (
-	id integer NOT NULL,
-	url varchar(400),
-	PRIMARY KEY (id),
-	FOREIGN KEY (id) REFERENCES Bookmark(id)
+    id integer NOT NULL,
+    url varchar(400),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES Bookmark(id)
 );
 
 CREATE TABLE IF NOT EXISTS Book (
-	id integer NOT NULL,
-	author varchar(100),
-	isbn varchar(20),
-	PRIMARY KEY (id),
-	FOREIGN KEY (id) REFERENCES Bookmark(id)
+    id integer NOT NULL,
+    author varchar(100),
+    isbn varchar(20),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES Bookmark(id)
 );
-
-/* TODO: Consider adding NOT NULL for some of the columns */
-/* TODO: Consider changing some varchar sizes */
